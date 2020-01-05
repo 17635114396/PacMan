b1fd9c74fca1321bfbce799e843fa26bbb9253ed
17635114396 <2769158060@qq.com>
2020-01-05 10:46:41 +0800

    灯光贴图管理脚本

---
+++ Assets/C#Scripts/MyLightManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLightManager : MonoBehaviour
+{
+    public Light[] allLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 关灯
+    /// </summary>
+    /// <param name="i"></param>
+    public void MyCloseLight(int i) {
+        allLight[i].GetComponent<Light>().enabled = false;
+    }
+
+    /// <summary>
+    /// 开灯
+    /// </summary>
+    /// <param name="i"></param>
+    public void MyOpenLight(int i)
+    {
+        allLight[i].GetComponent<Light>().enabled = true;
+    }
+}
---
+++ Assets/C#Scripts/MyMaterialManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMaterialManager : MonoBehaviour
+{
+    public Material[] allMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 替换贴图
+    /// </summary>
+    /// <param name="go">替换的物体</param>
+    /// <param name="i">替换的贴图编号</param>
+    public void ReplaceMaterial(GameObject go,int i)
+    {
+        go.GetComponent<Renderer>().material = allMaterial[i];
+    }
+}
