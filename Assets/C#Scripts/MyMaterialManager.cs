5d740f78c48efe86672d66a794db7474e8466936
17635114396 <2769158060@qq.com>
2020-01-05 11:36:16 +0800

    背景切换已搞定

--- Assets/C#Scripts/MyMaterialManager.cs
+++ Assets/C#Scripts/MyMaterialManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MyMaterialManager : MonoBehaviour
 {
     public Material[] allMaterial;
+    public GameObject[] go;
+    int i = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +27,28 @@ public class MyMaterialManager : MonoBehaviour
     public void ReplaceMaterial(GameObject go,int i)
     {
         go.GetComponent<Renderer>().material = allMaterial[i];
+    }
+
+    /// <summary>
+    /// 根据不同的选项更换地图贴图
+    /// </summary>
+    public void Ocean()
+    {
+        i = 4;
+        ReplaceMaterial(go[0], i);
+    }
+    public void Grass() {
+        i = 1;
+        ReplaceMaterial(go[0], i);
+    }
+    public void Desirt()
+    {
+        i = 3;
+        ReplaceMaterial(go[0], i);
+    }
+    public void Ston()
+    {
+        i = 2;
+        ReplaceMaterial(go[0], i);
     }
 }
